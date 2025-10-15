9a2bcfa22ebd5bb7432d30745c7ebb9667096ce7
MUHAMMAD BAQAR LAGHARI <lagharimbaqar@gmail.com>
2025-10-15 09:39:32 +0500

    Add files via upload

---
+++ DS-LAB6/t1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Stack
+{
+private:
+    char arr[100];
+    int top;
+
+public:
+    Stack()
+    {
+        top = -1;
+    }
+
+    void push(char c)
+    {
+        if (top >= 99)
+        {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        arr[++top] = c;
+    }
+
+    char pop()
+    {
+        if (top < 0)
+        {
+            cout << "Stack Underflow" << endl;
+            return '\0';
+        }
+        return arr[top--];
+    }
+
+    bool isEmpty()
+    {
+        return top < 0;
+    }
+};
+
+int main()
+{
+    string str = "BORROWROB";
+    Stack s;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        s.push(str[i]);
+    }
+
+    string reversed = "";
+    while (!s.isEmpty())
+    {
+        reversed += s.pop();
+    }
+
+    cout << reversed << endl;
+
+    if (str == reversed)
+    {
+        cout << "PALINDROME" << endl;
+    }
+    else
+    {
+        cout << "PALINDROME" << endl;
+    }
+
+    return 0;
+}
